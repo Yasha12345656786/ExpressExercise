8b8043bd7212b5355ef8ffbc6ca3beb4f97d3d0f
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-04-17 20:08:59 +0300

    midExUpdate

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path =require('node:path');
+
+const PORT = process.env.PORT || 8000;
+
+const server = express();
+server.use(express.json());
+
+server.use(`/api/store`,require('./Routes/stores'));
+
+server.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`); 
+});
